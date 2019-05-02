0e9194cbb21154872667be29ecca147e2d2a87ba
Grant Stoltz <grant@grantstoltz.com>
2019-05-01 23:48:00 -0700

    Added BigQuery interaction

--- Code.js
+++ Code.js
@@ -1,11 +1,78 @@
 /**
+ * Loads a message into BigQuery
+ */
+function loadMessage(message) {
+  // Replace this value with the project ID listed in the Google
+  // Cloud Platform project.
+  var projectId = 'tinobot';
+  // Create a dataset in the BigQuery UI (https://bigquery.cloud.google.com)
+  // and enter its ID below.
+  var datasetId = 'messages';
+
+  // Create the table.
+  var tableId = 'messages';
+
+  function bigqueryInsertData(data, tableId) {
+    var insertAllRequest = BigQuery.newTableDataInsertAllRequest();
+
+    insertAllRequest.rows = [];
+
+    var row1 = BigQuery.newTableDataInsertAllRequestRows();
+    row1.json = {
+      message: data
+    };
+    insertAllRequest.rows.push(row1);
+
+    var response = BigQuery.Tabledata.insertAll(
+      insertAllRequest,
+      projectId,
+      datasetId,
+      tableId
+    );
+    if (response.insertErrors) {
+      Logger.log(response.insertErrors);
+    }
+  }
+  bigqueryInsertData(message, tableId);
+}
+
+function queryDataTable() {
+  // Replace this value with the project ID listed in the Google
+  // Cloud Platform project.
+  var projectId = 'tinobot';
+
+  var dataSetId = 'messages';
+  var tableId = 'messages';
+
+  var fullTableName = projectId + ':' + dataSetId + '.' + tableId;
+
+  var queryRequest = BigQuery.newQueryRequest();
+  queryRequest.query =
+    'SELECT *, RAND() AS r FROM [' + fullTableName + '] ORDER BY r LIMIT 1;';
+  var query = BigQuery.Jobs.query(queryRequest, projectId);
+
+  if (query.jobComplete) {
+    Logger.log(query.rows[0].f[0].v);
+    return query.rows[0].f[0].v;
+  }
+}
+
+/**
  * Responds to a MESSAGE event in Hangouts Chat.
  *
  * @param {Object} event the event object from Hangouts Chat
  */
 function onMessage(event) {
-    var message = "You just said: \"" + event.message.text + "\"";
-    return { "text": message };
+  Logger.log(event.message.sender.displayName);
+  var message = event.message.text;
+  // loadMessage(message);
+
+  if (message === 'random') {
+    var randomSaying = queryDataTable();
+    return { text: randomSaying };
+  } else {
+    return;
+  }
 }
 /**
  * Responds to an ADDED_TO_SPACE event in Hangouts Chat.
@@ -13,20 +80,21 @@ function onMessage(event) {
  * @param {Object} event the event object from Hangouts Chat
  */
 function onAddToSpace(event) {
-  var message = "";
+  var message = '';
 
-  if (event.space.type == "DM") {
-    message = "Thank you for adding me to a DM, " + event.user.displayName + "!";
+  if (event.space.type == 'DM') {
+    message =
+      'Thank you for adding me to a DM, ' + event.user.displayName + '!';
   } else {
-    message = "Thank you for adding me to " + event.space.displayName;
+    message = 'Thank you for adding me to ' + event.space.displayName;
   }
 
   if (event.message) {
     // Bot added through @mention.
-    message = message + " and you said: \"" + event.message.text + "\"";
+    message = message + ' and you said: "' + event.message.text + '"';
   }
 
-  return { "text": message };
+  return { text: message };
 }
 
 /**
@@ -35,6 +103,5 @@ function onAddToSpace(event) {
  * @param {Object} event the event object from Hangouts Chat
  */
 function onRemoveFromSpace(event) {
-  console.info("Bot removed from ", event.space.name);
+  console.info('Bot removed from ', event.space.name);
 }
-
